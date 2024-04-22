ffd1b55bc8ec0bfd13cec069c9f3154870d3566d
Colson <colsoncalifornication@gmail.com>
2024-04-22 10:14:52 +0545

    initial project state

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import PageContent from '../components/PageContent';
+
+function HomePage() {
+  return (
+    <PageContent title='Welcome!'>
+      <h2 style={{ color: 'orangered' }}>jwt Authentication REACT</h2>
+      <p>Browse all our amazing events!</p>
+    </PageContent>
+  );
+}
+
+export default HomePage;
