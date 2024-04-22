3238abddaa3fdb6f791f26f2b4b1da44b23c0091
Colson <colsoncalifornication@gmail.com>
2024-04-22 13:39:35 +0545

    add user logout

---
+++ frontend/src/pages/Logout.js
@@ -0,0 +1,10 @@
+// This file won't contain any component because there is no log out page.
+// Instead we'll export a function which will clear out local storage
+
+import { redirect } from 'react-router-dom';
+
+// i.e get rid of token
+export function action() {
+  localStorage.removeItem('token');
+  return redirect('/');
+}
